519c371a873dfa5816ff971aa5e646df0e6fbfad
Kelleymb <kelleybuccieri@gmail.com>
2020-07-23 12:02:11 -0400

    testing bugs

--- src/Reviews/Reviews.test.js
+++ src/Reviews/Reviews.test.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import {shallow} from 'enzyme';
 import Reviews from './Reviews';
 
+var enzyme = require('enzyme');
+import Adapter from 'enzyme-adapter-react-16';
+enzyme.configure({ adapter: new Adapter() });
+
 describe(`Reviews Component`, () => {
     
     it(`renders without errors`, () => {
@@ -11,10 +15,19 @@ describe(`Reviews Component`, () => {
         ReactDOM.unmountComponentAtNode(section);
     });
 
-    it(`onSubmit callback is fired when form is submitted`, () => {
+    it(`Rating Search, onSubmit callback is fired when form is submitted`, () => {
         const callback = jest.fn();
         const wrapper = shallow(<Reviews onSubmit={callback}/>);
-        wrapper.find('button').simulate('click');
+        wrapper.update()
+        wrapper.find('rating-search-form').simulate('click');
+        expect(callback).toHaveBeenCalled();
+    });
+
+    it(`User Search, onSubmit callback is fired when form is submitted`, () => {
+        const callback = jest.fn();
+        const wrapper = shallow(<Reviews onSubmit={callback}/>);
+        wrapper.update()
+        wrapper.find('user-search-form').simulate('click');
         expect(callback).toHaveBeenCalled();
     });
 })
