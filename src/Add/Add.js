bf7be56164a2e29699fc7adec203d6e4b3b318cf
Kelleymb <kelleybuccieri@gmail.com>
2020-08-04 22:23:57 -0400

    Add 640px & 800px

--- src/Add/Add.js
+++ src/Add/Add.js
@@ -72,7 +72,6 @@ class Add extends Component {
                     <h1>Add a Review</h1>
                     <h2>You are entering a spoiler free zone</h2>
                     <section className="guide-section">
-                        <h4>Let us know...</h4>
                         <h4>Would you recommend this book to others?</h4>
                         <h4>Did it live up to your expectations?</h4>
                         <h4>Did you laugh? Cry? Think?</h4>
@@ -83,39 +82,37 @@ class Add extends Component {
                         <h4>Do you want to read it again?</h4>
                     </section>
                     <form className="add-review-form" onSubmit={this.handleSubmit}>
-                        <div className="field">
+                        <div className="user-field">
                             <label className="user-input" htmlFor="user-input" aria-required="true">Username</label>
-                            {/* <p className="user-input">Make sure your credentials match!</p> */}
                             <input type="text" id="user-input" name="user-input"/>
                         </div>
-                        <div className="field">
+                        <div className="title-field">
                             <label className="title-input" htmlFor="title-input" aria-required="true">Title</label>
                             <input type="text" id="title-input" name="title-input"/>
                         </div>
-                        <div className="field">
+                        <div className="author-field">
                             <label className="author-input" htmlFor="author-input" aria-required="true">Author</label>
                             <input type="text" id="author-input" name="author-input"/>
                         </div>
-                        <div className="field">
+                        <div className="content-field">
                             <label className="add-content-input" htmlFor="add-content-input" aria-required="true">Content</label>
                             <textarea id="add-content-input" name="add-content"></textarea>
                         </div>
-                        <div className="field">
-                            <section className="rating-section">
-                                <h3>Qualities of a...</h3>
-                                <h4>5 star rating</h4>
+                        <div className="rating-field">
+                            <h3>Qualities of a...</h3>
+                            <section className="rating-guide">
+                                <h4 className="five-star">5 star rating</h4>
                                     <h5>An absolute masterpiece, impactful, a must read</h5>
-                                <h4>4 star rating</h4>
+                                <h4 className="four-star">4 star rating</h4>
                                     <h5>A quality work, highly recommend</h5>
-                                <h4>3 star rating</h4>
+                                <h4 className="three-star">3 star rating</h4>
                                     <h5>Worth a read, would recommend</h5>
-                                <h4>2 star rating</h4>
-                                    <h5>Meh</h5>
-                                <h4>1 star rating</h4>
-                                    <h5>Would not read again, would not recommend</h5>
+                                <h4 className="two-star">2 star rating</h4>
+                                    <h5>Did not meet my expectations, would not reccomend</h5>
+                                <h4 className="one-star">1 star rating</h4>
+                                    <h5>Read at your own risk</h5>
                             </section>
-                            <h4>Given thought to the rating parameters above, what rating would you give to this piece?</h4>
-                            <label className="rating-input" htmlFor="rating-input" aria-required="true">Rating</label>
+                            <label className="rating-input" htmlFor="rating-input" aria-required="true">Given thought to the rating parameters above, what rating would you give to this piece?</label>
                             <select id="rating-input" name="rating-input">
                                 <option value="1">1</option>
                                 <option value="2">2</option>
